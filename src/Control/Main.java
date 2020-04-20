327956db3f5254ab65bffa76eea802ac2d77c089
SpikeYr <emil.f.anderson@gmail.com>
2020-04-20 10:01:45 +0200

    Initial commit

---
+++ src/Control/Main.java
@@ -0,0 +1,21 @@
+package Control;
+
+// Kopierat från din MVC sample
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception{
+		primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("View"))));
+		primaryStage.setTitle("Tetris");
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
